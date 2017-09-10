54db49db0d6fee4a8751780a5867e3e4f93419d3
Gad Berger <gad.berger@gmail.com>
2017-09-10 11:22:00 -0400

    Updated the test script to just use get-date

--- Modules/LocalToUtc/Tests/LocalToUtc.Tests.ps1
+++ Modules/LocalToUtc/Tests/LocalToUtc.Tests.ps1
@@ -12,14 +12,14 @@ Describe 'LocalToUtc' {
     
     It 'Parses ISO8601 properly' {
         $result = Convert-LocalToUTC -LocalTime $testTime -Verbose
-        $expected = [System.DateTime]::Parse($testTime)
+        $expected = Get-Date $testTime
         $result.LocalTime | Should Be $expected
     }
 
     It 'Converts local time to UTC time when the local time is defined' {
         $result = Convert-LocalToUTC -LocalTime $testTime -TimeZone "Eastern Standard Time" -Verbose
-        $expectedUtc = [System.DateTime]::Parse($testTime).AddHours(4)
-        $expectedLocal = [System.DateTime]::Parse($testTime)
+        $expectedUtc = (Get-Date $testTime).AddHours(4)
+        $expectedLocal = Get-Date $testTime
         $result.UtcTime | Should Be $expectedUtc
         $result.LocalTime | Should Be $expectedLocal
     }
@@ -34,20 +34,20 @@ Describe 'LocalToUtc' {
             return $cst
         }
         Mock -ModuleName LocalToUtc Get-UtcTime {
-            $utcTime = [System.DateTime]::Parse("2017-09-04 08:25:00")
+            $utcTime = Get-Date "2017-09-04 08:25:00"
             $utc = [System.DateTime]::SpecifyKind($utcTime, [System.DateTimeKind]::Utc)
             return $utc
         }
         Mock -ModuleName LocalToUtc Get-LocalTime {
             $cstTime = "2017-09-04 03:25:00"
-            $localTime = [System.DateTime]::Parse($cstTime)
+            $localTime = Get-Date $cstTime
             return $localTime
         }
 
         $utcTime = "2017-09-04 08:25:00"
         $pstTime = "2017-09-04 01:25:00" # PDT is 7 hours behind UTC (daylight savings)
-        $expectedUtc = [System.DateTime]::Parse($utcTime)
-        $expectedLocal = [System.DateTime]::Parse($pstTime)
+        $expectedUtc = Get-Date $utcTime
+        $expectedLocal = Get-Date $pstTime
 
         $result = Convert-LocalToUTC -TimeZone "Pacific Standard Time" -Verbose
         $result.UtcTime | Should Be $expectedUtc
@@ -56,13 +56,13 @@ Describe 'LocalToUtc' {
 
     It 'Adds hours, minutes, seconds correclty' {
         $result = Convert-LocalToUTC $testTime -AddDays 1 -AddHours 2 -AddMinutes 3 -Verbose
-        $expected = [System.DateTime]::Parse($testTime).AddDays(1).AddHours(2).AddMinutes(3)
+        $expected = (Get-Date $testTime).AddDays(1).AddHours(2).AddMinutes(3)
         $result.LocalTime | Should Be $expected
     }
 
     It 'Has the timezone as the first unnamed parameter' {
-        $localTime = [System.DateTime]::Parse($testTime)
-        $utc = [System.DateTime]::Parse($testTime).AddHours(4) # 4 hours ahead for EDT
+        $localTime = Get-Date $testTime
+        $utc = (Get-Date $testTime).AddHours(4) # 4 hours ahead for EDT
         $result = Convert-LocalToUtc $localTime "Eastern Standard Time"
         $result | Should Be $utc
     }
@@ -73,33 +73,33 @@ Describe 'UtcToLocal' {
     
     It 'Parses ISO8601 properly' {
         $result = Convert-UtcToLocal -UtcTime $testTime -Verbose
-        $expected = [System.DateTime]::Parse($testTime)
+        $expected = Get-Date $testTime
         $result.UtcTime | Should Be $expected
     }
 
     It 'Converts UTC time to local time correctly' {
         $result = Convert-UtcToLocal -UtcTime $testTime -TimeZone "Eastern Standard Time"
-        $expected = [System.DateTime]::Parse($testTime).AddHours(-4)
+        $expected = (Get-Date $testTime).AddHours(-4)
         $result | Should Be $expected
     }
 
     It 'Adds hours, minutes, seconds correclty' {
         $result = Convert-UtcToLocal $testTime "Eastern Standard Time" -AddDays 1 -AddHours 2 -AddMinutes 3 -Verbose
-        $expected = [System.DateTime]::Parse($testTime).AddDays(1).AddHours(2).AddMinutes(3)
+        $expected = (Get-Date $testTime).AddDays(1).AddHours(2).AddMinutes(3)
         $result.UtcTime | Should Be $expected
     }
 
     It 'Has the timezone as the first unnamed parameter' {
-        $utc = [System.DateTime]::Parse($testTime)
-        $local = [System.DateTime]::Parse($testTime).AddHours(-4) # 4 hours behind for EDT
+        $utc = Get-Date $testTime
+        $local = (Get-Date $testTime).AddHours(-4) # 4 hours behind for EDT
         $result = Convert-UtcToLocal $utc "Eastern Standard Time"
         $result | Should Be $local
     }
 
     It 'Verbose returns the full object' {
         $pst = Get-TimeZone "Pacific Standard Time"
-        $result = Convert-UtcToLocal $testTime "Pacific Standard Time" -Verbose
-        $utc = [System.DateTime]::Parse($testTime)
+        $result = Convert-UtcToLocal (Get-Date $testTime) "Pacific Standard Time" -Verbose
+        $utc = Get-Date $testTime
         $local = $utc.AddHours(-7)
         $result.LocalTime | Should Be $local
         $result.UtcTime | Should Be $utc
