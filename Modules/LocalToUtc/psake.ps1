2358f768c0f060e566cd1b572a7f4e8a3a37508e
Gad Berger <gad.berger@gmail.com>
2017-09-10 11:21:12 -0400

    adding psake scripts

---
+++ Modules/LocalToUtc/psake.ps1
@@ -0,0 +1,48 @@
+
+Properties {
+    $ProjectRoot = $PSScriptRoot
+    $PSVersion = $PSVersionTable.PSVersion.Major
+    $Timestamp = Get-date -uformat "%Y%m%d-%H%M%S"
+    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
+    $lines = "----------------------------------------------------------------------"
+    $newLine = "`n"
+}
+
+Task Default -Depends Deploy
+
+Task Init {
+    $lines
+    "init"
+    
+    $newLine
+}
+
+Task Test -Depends Init {
+    $lines
+    "STATUS: Testing with PowerShell $PSVersion"
+
+    # Gather test results. Store them in a variable and file
+    $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile"
+    
+    Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
+
+    # Failed tests?
+    # Need to tell psake or it will proceed to the deployment. Danger!
+    "Test Result Failed Count - $TestResults.FailedCount"
+    if($TestResults.FailedCount -gt 0)
+    {
+        Write-Error "Failed '$($TestResults.FailedCount)' tests, build failed"
+        "this is bs!"
+    }
+
+    $newLine
+}
+
+Task Deploy -Depends Test {
+    $lines
+    "deploy"
+
+    #Publish-Module -Name LocalToUtc -NugetApiKey $ENV:NugetApiKey
+
+    $newLine
+}
