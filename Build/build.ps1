99c0a188d6a951079b2bdf52cbe063c7f681511e
Gad Berger <gad.berger@gmail.com>
2017-09-10 12:37:21 -0400

    uncommenting import/install statemetns

--- Build/build.ps1
+++ Build/build.ps1
@@ -3,11 +3,11 @@
 param ($Task = 'Default')
 
 # Grab nuget bits, install modules, set build variables, start build.
-#Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-#Install-Module Psake, PSDeploy -force
-#Install-Module Pester -Force -SkipPublisherCheck
-#Import-Module Psake
+Install-Module Psake, PSDeploy -force
+Install-Module Pester -Force -SkipPublisherCheck
+Import-Module Psake
 
 Invoke-psake -buildFile $PSScriptRoot\psake.ps1 -taskList $Task -nologo
 exit ( [int]( -not $psake.build_success ) )
