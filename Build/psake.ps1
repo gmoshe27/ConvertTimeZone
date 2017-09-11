46e3b5c64e8c83c089da98fd1e08d2afbe24d60d
Gad Berger <gad.berger@gmail.com>
2017-09-10 21:55:02 -0400

    removed the test result file output

--- Build/psake.ps1
+++ Build/psake.ps1
@@ -2,8 +2,6 @@
 Properties {
     $ProjectRoot = Resolve-Path "$PSScriptRoot\.."
     $PSVersion = $PSVersionTable.PSVersion.Major
-    $Timestamp = Get-date -uformat "%Y%m%d-%H%M%S"
-    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
     $lines = "----------------------------------------------------------------------"
     $newLine = "`n"
 }
@@ -15,9 +13,7 @@ Task Test {
     "STATUS: Testing with PowerShell $PSVersion"
 
     # Gather test results. Store them in a variable and file
-    $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile"
-    
-    Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
+    $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru
 
     # Failed tests?
     # Need to tell psake or it will proceed to the deployment. Danger!
@@ -31,9 +27,6 @@ Task Test {
 
 Task Deploy -Depends Test {
     $lines
-
-    $IsTagged = $env:APPVEYOR_REPO_TAG -eq $true
-    $HostIsAppveyor = $env:APPVEYOR -eq $true
 
     "Deploying to powershell gallery due to tag - $env:APPVEYOR_REPO_TAG_NAME"
     Publish-Module -Path $ProjectRoot\LocalToUtc -NugetApiKey $ENV:NugetApiKey
